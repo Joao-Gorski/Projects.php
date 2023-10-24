21c66de5a7b451b47ccc07149526fedf3723d1ab
Joao-Gorski <joaogorski8@gmail.com>
2023-10-24 13:24:45 -0300

    first commit

--- Treino.php/cad.php
+++ Treino.php/cad.php
@@ -3,28 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Treino_PhP</title>
     <link rel="stylesheet" href="/Treino.php/style.css">
 </head>
 <body>
-    <h1>Escolhe o horario</h1>
-   <main>
-<form action="test.php" method="$_GET">
-    <?php 
+<main>
     
-     $n = $_GET["nome"] ?? "desconhecido";
-     $s = $_GET["sobrenome"] ?? "Sem nome";//hyper-text preprocesseser// pre processamento de hiper texto
-     echo "<p>SEJA BEM VINDO, <strong>$n $s </strong>!";
-     echo "<button> 18:40";
-     echo "<button> 19:00";
-     echo "<button>  20:00";
-    
-    
-    ?>
-</form>
+    <h1>Conversor de moedas</h1>
+<?php 
+$inicio = date("m-d-Y", strtotime("-7 days")) ;
+$fim = date("m-d-Y") ;
+ $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=%27'. $inicio .'%27&@dataFinalCotacao=%27' . $fim . '%27&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+ $dados = json_decode(file_get_contents($url), true);
+
+ //var_dump($dados);
+
+ $cotação = $dados["value"][0] ["cotacaoCompra"];  
+ 
+ $real = $_GET["din"] ?? 0;
+
+ $dolar = $real / $cotação;
+
+ $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+ echo "<p>Seus ", numfmt_format_currency($padrão, $real, "BRL"), "  equivalem a <strong>" . numfmt_format_currency($padrão, $dolar, "USD") . "</strong></p>" . "<p> Usando a cotação atual do <a href = https://www.bcb.gov.br/> Banco Central</a></p>" ;     
+ ?>
+ <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
-
-
-
 </body>
 </html>
