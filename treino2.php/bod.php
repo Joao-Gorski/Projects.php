ef2529f1dfb0e60057ae041d939120a6f0cd13f3
Joao-Gorski <joaogorski8@gmail.com>
2023-11-01 12:42:58 -0300

    second server

--- treino2.php/bod.php
+++ treino2.php/bod.php
@@ -1,24 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/treino2.php/style.css">
 </head>
+
 <body>
-    
+
+  <?php
+  $valor = $_GET ['seg'] ?? 0;
+  ?>
+
   <main>
-    <pre>
-<?php   
-
-echo "<H1>SuperGlobal Server</h1>";
-var_dump($_SERVER);
-
-?>
-
-  </pre>
-
+    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+      <label for="seg">Valor de segundos</label>
+      <input type="number" name="seg" id="seg" value="<?= $valor ?>">
+      <input type="submit" value="Calcular">
+    </form>
   </main>
 
+<section id="Soma">
+  <h2>Resultado</h2>
+<?php 
+$sobra = $valor;
+//Total de Semanas
+$semana = (int)($sobra / 604_800);
+$sobra = $sobra % 604_800;
+//Dias
+$dias = (int)($sobra / 86_400);
+$sobra = $sobra % 86_400;
+//Horas
+$hora = (int) ($sobra / 3_600);
+$sobra = $sobra % 3_600;
+//Minutos
+$minutos = (int) ($sobra / 60);
+$sobra = $sobra % 60;
+//Segundos
+$segundos =$sobra;
+?>
+</section>
+<section>
+<h2>Totalizando tudo</h2>
+<p>Analisando o valor <?=$valor?></p>
+<ul>
+  <li><?=$semana ?> semanas</li>
+  <li><?=$dias ?> dias</li>
+  <li><?=$hora?> Horas</li>
+  <li><?=$minutos?> Minutos</li>
+  <li><?=$segundos?> Segundos</li>
+
+
+</ul>
+
+
+</section>
+
 </body>
+
 </html>
